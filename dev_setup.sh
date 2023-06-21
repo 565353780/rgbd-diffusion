6633f284d8d219accb86cb59cb59e37101b808bc
chLi <565353780@qq.com>
2023-06-21 19:42:28 +0800

    add setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,13 @@
+pip install matplotlib opencv-python einops \
+	trimesh diffusers ninja open3d
+
+pip install torch torchvision torchaudio
+
+cd ./third_party/latent-diffusion/
+pip install .
+
+cd ../nvdiffrast/
+pip install .
+
+cd ../recon/
+pip install .
