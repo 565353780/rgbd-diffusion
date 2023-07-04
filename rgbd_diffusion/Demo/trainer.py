9a292a7f97e7314e254aaeea4e2fcc820649695c
chLi <565353780@qq.com>
2023-07-04 20:53:55 +0800

    add resume_model_only param

--- rgbd_diffusion/Demo/trainer.py
+++ rgbd_diffusion/Demo/trainer.py
@@ -4,9 +4,9 @@ from rgbd_diffusion.Module.trainer import Trainer
 def demo():
     dataset_folder_path = '/home/chli/chLi/RGBD_Diffusion/scans_keyframe/'
     model_file_path = '/home/chli/chLi/RGBD_Diffusion/model.pt'
-    resume_model_only = False
+    resume_model_only = True
     print_progress = True
 
-    trainer = Trainer(dataset_folder_path, model_file_path)
+    trainer = Trainer(dataset_folder_path, model_file_path, resume_model_only)
     trainer.train(print_progress)
     return True
